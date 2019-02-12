6499154f1dc99b0105276abced25db2378e8f143
erevos <orfeasvou13@hotmail.com>
2019-02-12 23:47:40 +0200

    add traslation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,13 @@ import {HomePageComponent} from './pages/home-page/home-page.component';
 import {AppRoutingModule} from './app-routing.module';
 import { LogListComponent } from './pages/log-list/log-list.component';
 import {JumbotronComponent} from './modules/jumbotron/jumbotron.component';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 
 @NgModule({
@@ -18,7 +24,16 @@ import {JumbotronComponent} from './modules/jumbotron/jumbotron.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
