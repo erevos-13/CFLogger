2f31d31ace90877c6b3ed9e1c319f472a5a457a0
orfeas <orfeasvou13@hotmail.com>
2019-02-27 22:44:43 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   URL_DEV: 'http://localhost:3000',
   production: false,
-  firebase: {
-    apiKey: "AIzaSyD7iJJVUg-2AtQl_nweaCPpfL7yXjq3iRw",
-    authDomain: "pragmatic-port-215917.firebaseapp.com",
-    databaseURL: "https://pragmatic-port-215917.firebaseio.com",
-    projectId: "pragmatic-port-215917",
-    storageBucket: "pragmatic-port-215917.appspot.com",
-    messagingSenderId: "300660854360"
-  }
+ 
 };
 
 /*
